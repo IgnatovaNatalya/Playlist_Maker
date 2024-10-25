77eafa046a8d54308a573051ef5da3b452b1161b
IgnatovaNatalya <ananjeva@gmail.com>
2024-10-25 12:45:15 +0300

    Кнопки нажимаем

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+
+        val btnSearch = findViewById<Button>(R.id.main_search_button)
+        val btnMedia = findViewById<Button>(R.id.main_media_button)
+        val btnSettings = findViewById<Button>(R.id.main_settings_button)
+
+        btnSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск", Toast.LENGTH_SHORT).show()
+        }
+
+        btnMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиа", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
