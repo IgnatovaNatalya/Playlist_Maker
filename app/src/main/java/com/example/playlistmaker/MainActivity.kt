272b6498ac36a9d89da0623204ee6aebd972645c
IgnatovaNatalya <ananjeva@gmail.com>
2024-10-26 22:17:09 +0300

    Навигация

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,27 +15,27 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val btnSearch = findViewById<Button>(R.id.main_search_button)
         val btnMedia = findViewById<Button>(R.id.main_media_button)
         val btnSettings = findViewById<Button>(R.id.main_settings_button)
 
+
         btnSearch.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this,SearchActivity::class.java))
         }
 
         btnMedia.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиа", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this,MediaActivity::class.java))
         }
 
         btnSettings.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this,SettingsActivity::class.java))
         }
     }
 }
