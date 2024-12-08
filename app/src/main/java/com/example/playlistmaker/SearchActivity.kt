973f913e759358df16c208ad8178dd3bc255fc49
IgnatovaNatalya <ananjeva@gmail.com>
2024-12-08 22:48:17 +0300

    начало 11 спринта

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,9 +17,32 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
+
+
+    private lateinit var clearButton: ImageView
+    private lateinit var searchField: EditText
+    private lateinit var placeholderMessage: TextView
+    private lateinit var recyclerTracks : RecyclerView
+
+    private val baseUrl = "https://itunes.apple.com"
+
+    private val trackList = ArrayList<Track>()
+    private var trackAdapter = TrackAdapter()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,9 +57,12 @@ class SearchActivity : AppCompatActivity() {
         val toolbar = findViewById<MaterialToolbar>(R.id.search_toolbar)
         toolbar.setNavigationOnClickListener {  finish() }
 
+        val iTunesService = retrofit.create(iTunesApi::class.java)
+
         //search field
-        val searchField = findViewById<EditText>(R.id.search_input_text)
-        val clearButton = findViewById<ImageView>(R.id.search_clear_button)
+        searchField = findViewById<EditText>(R.id.search_inpit_text)
+        clearButton = findViewById<ImageView>(R.id.search_clear_button)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
 
         clearButton.setOnClickListener {
             searchField.setText(DEFAULT_TEXT)
@@ -53,11 +82,60 @@ class SearchActivity : AppCompatActivity() {
         searchField.addTextChangedListener(simpleTextWatcher)
 
         //recycler
-        val recyclerView = findViewById<RecyclerView>(R.id.search_recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val trackAdapter = TrackAdapter(trackList)
-        recyclerView.adapter = trackAdapter
+        recyclerTracks = findViewById(R.id.search_recycler)
+        recyclerTracks.layoutManager = LinearLayoutManager(this)
 
+        trackAdapter.tracks = trackList
+        recyclerTracks.adapter = trackAdapter
+
+        fun search() {
+            iTunesService.search( searchField.text.toString())
+                .enqueue(object : Callback<TracksResponse> {
+                    override fun onResponse(call: Call<TracksResponse>,
+                                            response: Response<TracksResponse>
+                    ) {
+                        when (response.code()) {
+                            200 -> {
+                                if (response.body()?.results?.isNotEmpty() == true) {
+                                    trackList.clear()
+                                    trackList.addAll(response.body()?.results!!)
+                                    trackAdapter.notifyDataSetChanged()
+                                    showMessage("", "")
+                                } else {
+                                    showMessage(getString(R.string.nothing_found), "")
+                                }
+                            }
+                            else -> showMessage(getString(R.string.something_went_wrong), response.code().toString())
+                        }
+                    }
+                    override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                        showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                    }
+
+                })
+        }
+
+        searchField.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+            }
+            false
+        }
+    }
+
+    private fun showMessage(text: String, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            trackList.clear()
+            trackAdapter.notifyDataSetChanged()
+            placeholderMessage.text = text
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+        }
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -82,9 +160,4 @@ class SearchActivity : AppCompatActivity() {
         const val ENTERED_TEXT = "ENTERED_TEXT"
         const val DEFAULT_TEXT = ""
     }
-
-
-
-
-
 }
---
+++ app/src/main/java/com/example/playlistmaker/iTunesApi.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker
+
+import retrofit2.Call
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface iTunesApi {
+    @POST("/search?entity=song")
+    fun search(@Query("term") text: String): Call<TracksResponse>
+
+}
